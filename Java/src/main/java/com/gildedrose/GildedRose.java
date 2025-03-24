75c41a107cdcb14bc896ee8eed91dd3a976a6b2f
pranjalS9 <p.arudiya9@gmail.com>
2025-03-24 19:05:33 +0530

    Convert if else conditions into switch based on the item name

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,30 +10,33 @@ class GildedRose {
     public void updateQuality() {
         for (Item item : items) {
 
-            if(item.name.equals("Sulfuras, Hand of Ragnaros")) continue;
+            switch (item.name) {
+                case "Sulfuras, Hand of Ragnaros":
+                    continue;
+                case "Aged Brie":
+                    if (item.quality < 50) item.quality++;
+                    item.sellIn--;
+                    if (item.sellIn < 0 && item.quality < 50) item.quality++;
+                    break;
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    if (item.quality < 50) {
+                        item.quality++;
 
-            if(item.name.equals("Aged Brie")) {
-                if(item.quality < 50) item.quality++;
-                item.sellIn--;
-                if(item.sellIn < 0 && item.quality < 50) item.quality++;
-            }
-            else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if(item.quality < 50) {
-                    item.quality++;
-
-                    if(item.quality < 50) {
-                        if (item.sellIn < 11) item.quality++;
-                        if (item.sellIn < 6) item.quality++;
+                        if (item.quality < 50) {
+                            if (item.sellIn < 11) item.quality++;
+                            if (item.sellIn < 6) item.quality++;
+                        }
                     }
-                }
-                item.sellIn--;
-                if(item.sellIn < 0) item.quality = 0;
+                    item.sellIn--;
+                    if (item.sellIn < 0) item.quality = 0;
+                    break;
+                default:
+                    if (item.quality > 0) item.quality--;
+                    item.sellIn--;
+                    if (item.sellIn < 0 && item.quality > 0) item.quality--;
+                    break;
             }
-            else {
-                if(item.quality > 0) item.quality--;
-                item.sellIn--;
-                if(item.sellIn < 0 && item.quality > 0) item.quality--;
-            }
+
         }
     }
 }
