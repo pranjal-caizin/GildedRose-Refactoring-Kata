64a54f320d598436e26a314ee564b4513528ccd6
pranjalS9 <p.arudiya9@gmail.com>
2025-03-24 15:16:56 +0530

    Update test case which verify quality and sellin never changes for Sulfuras

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -41,9 +41,10 @@ class GildedRoseTest {
     }
 
     @Test
-    void qualityAndSellinDoesNotDecreaseForSulfurasTest() {
+    void qualityAndSellinDoesNotChangeForSulfurasTest() {
         Item[] items = new Item[] {
-            new Item("Sulfuras, Hand of Ragnaros", 2, 4)
+            new Item("Sulfuras, Hand of Ragnaros", 2, 4),
+            new Item("Sulfuras, Hand of Ragnaros", -1, 4)
         };
 
         GildedRose app = new GildedRose(items);
@@ -51,6 +52,9 @@ class GildedRoseTest {
 
         assertEquals(4, items[0].quality);
         assertEquals(2, items[0].sellIn);
+
+        assertEquals(4, items[1].quality);
+        assertEquals(-1, items[1].sellIn);
     }
 
     @Test
