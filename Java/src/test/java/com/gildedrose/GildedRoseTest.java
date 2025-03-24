00e682cd5c238a4cd648ae71ea37d9fa1e1b9222
pranjalS9 <p.arudiya9@gmail.com>
2025-03-24 12:19:06 +0530

    Add test case to verify quality and sellin does not decrease for Sulfuras

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -15,17 +15,33 @@ class GildedRoseTest {
     }
 
     @Test
-    void qualityDecreaseBy1ForNormalItemsTest() {
+    void qualityAndSellinDecreaseBy1ForNormalItemsTest() {
         Item[] items = new Item[] {
-            new Item("item 1", 2, 4),
-            new Item("item 2", 5, 7),
+            new Item("item 1", 2, 4)
         };
 
         GildedRose app = new GildedRose(items);
         app.updateQuality();
 
         assertEquals(3, items[0].quality);
-        assertEquals(6, items[1].quality);
+        assertEquals(1, items[0].sellIn);
+    }
+
+    @Test
+    void qualityAndSellinDoesNotDecreaseForSulfurasTest() {
+        Item[] items = new Item[] {
+            new Item("Sulfuras, Hand of Ragnaros", 2, 4)
+        };
+
+        GildedRose app = new GildedRose(items);
+
+        int days = 2;
+        for(int day = 0; day < days; day++) {
+            app.updateQuality();
+        }
+
+        assertEquals(4, items[0].quality);
+        assertEquals(2, items[0].sellIn);
     }
 
 }
