17d8bae81438f3b96d6f07bed9098b130a7816c1
pranjalS9 <p.arudiya9@gmail.com>
2025-03-24 19:01:33 +0530

    Merge all the conditions to make it eligible for switch (based on the item name)

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -14,7 +14,10 @@ class GildedRose {
 
             if(item.name.equals("Aged Brie")) {
                 if(item.quality < 50) item.quality++;
-            } else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                item.sellIn--;
+                if(item.sellIn < 0 && item.quality < 50) item.quality++;
+            }
+            else if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if(item.quality < 50) {
                     item.quality++;
 
@@ -23,14 +26,13 @@ class GildedRose {
                         if (item.sellIn < 6) item.quality++;
                     }
                 }
-            } else if (item.quality > 0) item.quality--;
-
-            item.sellIn--;
-
-            if (item.sellIn < 0) {
-                if(item.name.equals("Backstage passes to a TAFKAL80ETC concert")) item.quality = 0;
-                else if(item.name.equals("Aged Brie") && item.quality < 50) item.quality++;
-                else if(item.quality > 0) item.quality--;
+                item.sellIn--;
+                if(item.sellIn < 0) item.quality = 0;
+            }
+            else {
+                if(item.quality > 0) item.quality--;
+                item.sellIn--;
+                if(item.sellIn < 0 && item.quality > 0) item.quality--;
             }
         }
     }
