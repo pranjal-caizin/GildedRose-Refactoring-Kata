4c8ae849850d66e703657a620114ccc568e59fd6
pranjalS9 <p.arudiya9@gmail.com>
2025-03-25 12:35:53 +0530

    Add a test case which verify quality of Aged Brie does not exceed 50 AgedBrieTest class.

--- Java/src/test/java/com/gildedrose/AgedBrieTest.java
+++ Java/src/test/java/com/gildedrose/AgedBrieTest.java
@@ -9,8 +9,8 @@ public class AgedBrieTest {
     @Test
     void qualityIncreasesByOneBeforeSellDateTest() {
         Item item = new Item("Aged Brie", 5, 10);
-
         AgedBrie agedBrie = new AgedBrie(item);
+
         agedBrie.updateQuality();
 
         assertEquals(11, item.quality);
@@ -20,11 +20,22 @@ public class AgedBrieTest {
     @Test
     void qualityIncreasesTwiceAfterSellDateTest() {
         Item item = new Item("Aged Brie", 0, 10);
-
         AgedBrie agedBrie = new AgedBrie(item);
+
         agedBrie.updateQuality();
 
         assertEquals(12, item.quality);
         assertEquals(-1, item.sellIn);
+    }
+
+    @Test
+    void qualityDoesNotExceedFiftyTest() {
+        Item item = new Item("Aged Brie", 5, 50);
+        AgedBrie agedBrie = new AgedBrie(item);
+
+        agedBrie.updateQuality();
+
+        assertEquals(50, item.quality);
+        assertEquals(4, item.sellIn);
     }
 }
