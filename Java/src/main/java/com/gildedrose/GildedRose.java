780d2b7cde4989f5dc078873c9c329b0876dbacf
pranjalS9 <p.arudiya9@gmail.com>
2025-03-25 11:39:52 +0530

    Create constants of item names for switch cases

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -2,6 +2,9 @@ package com.gildedrose;
 
 class GildedRose {
     Item[] items;
+    private final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    private final String AGED_BRIE = "Aged Brie";
+    private final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
 
     public GildedRose(Item[] items) {
         this.items = items;
@@ -11,14 +14,14 @@ class GildedRose {
         for (Item item : items) {
 
             switch (item.name) {
-                case "Sulfuras, Hand of Ragnaros":
+                case SULFURAS:
                     continue;
-                case "Aged Brie":
+                case AGED_BRIE:
                     if (item.quality < 50) item.quality++;
                     item.sellIn--;
                     if (item.sellIn < 0 && item.quality < 50) item.quality++;
                     break;
-                case "Backstage passes to a TAFKAL80ETC concert":
+                case BACKSTAGE_PASSES:
                     if (item.quality < 50) {
                         item.quality++;
 
@@ -36,7 +39,6 @@ class GildedRose {
                     if (item.sellIn < 0 && item.quality > 0) item.quality--;
                     break;
             }
-
         }
     }
 }
