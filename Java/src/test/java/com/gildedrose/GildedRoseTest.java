7550da007d7c89399970ed216c0fc06f866e4720
pranjalS9 <p.arudiya9@gmail.com>
2025-03-27 11:01:14 +0530

    Add a test case to verify quality and sellin does not change for Sulfuras before the sell date

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -13,21 +13,4 @@ class GildedRoseTest {
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
-
-    @Test
-    void qualityAndSellinDoesNotChangeForSulfurasTest() {
-        Item[] items = new Item[] {
-            new Item("Sulfuras, Hand of Ragnaros", 2, 4),
-            new Item("Sulfuras, Hand of Ragnaros", -1, 4)
-        };
-
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-
-        assertEquals(4, items[0].quality);
-        assertEquals(2, items[0].sellIn);
-
-        assertEquals(4, items[1].quality);
-        assertEquals(-1, items[1].sellIn);
-    }
 }
---
+++ Java/src/test/java/com/gildedrose/SulfurasTest.java
@@ -0,0 +1,21 @@
+package com.gildedrose;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SulfurasTest {
+
+    @Test
+    void qualityAndSellinDoesNotChangeForSulfurasBeforeSellByDateTest() {
+        Item[] items = new Item[] {
+            new Item("Sulfuras, Hand of Ragnaros", 2, 4)
+        };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals(4, items[0].quality);
+        assertEquals(2, items[0].sellIn);
+    }
+}
