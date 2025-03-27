82623b0cc5acba34ccd2e5b2d2ed94c16a5f3610
pranjalS9 <p.arudiya9@gmail.com>
2025-03-27 13:05:57 +0530

    Extracted a method for decrease quality in NormalItem class

--- Java/src/main/java/com/gildedrose/NormalItem.java
+++ Java/src/main/java/com/gildedrose/NormalItem.java
@@ -7,8 +7,12 @@ public class NormalItem extends UpdatableItem {
 
     @Override
     void update() {
-        if (item.quality > 0) item.quality--;
+        decreaseQuality();
         item.sellIn--;
-        if (item.sellIn < 0 && item.quality > 0) item.quality--;
+        if (item.sellIn < 0) decreaseQuality();
+    }
+
+    private void decreaseQuality() {
+        if (item.quality > 0) item.quality--;
     }
 }
