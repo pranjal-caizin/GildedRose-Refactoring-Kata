95c66cfa79678ac14db74a6da76769911e9eaa7e
pranjalS9 <p.arudiya9@gmail.com>
2025-03-27 11:02:34 +0530

    Add a test case to verify quality and sellin does not change for Sulfuras after the sell date

--- Java/src/test/java/com/gildedrose/SulfurasTest.java
+++ Java/src/test/java/com/gildedrose/SulfurasTest.java
@@ -18,4 +18,17 @@ public class SulfurasTest {
         assertEquals(4, items[0].quality);
         assertEquals(2, items[0].sellIn);
     }
+
+    @Test
+    void qualityAndSellinDoesNotChangeForSulfurasAfterSellByDateTest() {
+        Item[] items = new Item[] {
+            new Item("Sulfuras, Hand of Ragnaros", -1, 4)
+        };
+
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+
+        assertEquals(4, items[0].quality);
+        assertEquals(-1, items[0].sellIn);
+    }
 }
